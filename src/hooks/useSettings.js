b9924c409317dd55cc21ec48835e407865764004
Khasoa <90859945+Khasoa@users.noreply.github.com>
2026-01-29 00:58:26 +0300

    Create useSettings.js

---
+++ src/hooks/useSettings.js
@@ -0,0 +1,29 @@
+import useLocalStorage from "./useLocalStorage";
+
+function useSettings() {
+    const [theme, setTheme] = useLocalStorage("theme", "light");
+    const [notificationsEnabled, setNotificationsEnabled] = useLocalStorage("notificationsEnabled", true);
+    
+    const toggleTheme = () => {
+        setTheme(theme === "light"? "dark": "light");
+    };
+
+    const toggleNotifications = () => {
+        setNotificationsEnabled(!notificationsEnabled);
+    };
+
+    const resetApp = () => {
+        localStorage.clear();
+        window.location.reload();
+    };
+
+    return {
+        theme,
+        notificationsEnabled,
+        toggleTheme,
+        toggleNotifications,
+        resetApp,
+    };
+}
+
+export default useSettings;
