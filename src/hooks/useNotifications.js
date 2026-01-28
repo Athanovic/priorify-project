17b9404e934bec9d5d54c11a65857d85e7f5b63a
Khasoa <90859945+Khasoa@users.noreply.github.com>
2026-01-29 00:55:33 +0300

    Create useNotifications.js

---
+++ src/hooks/useNotifications.js
@@ -0,0 +1,25 @@
+import {useEffect} from react;
+import useSettings from "./useSettings";
+
+function useNotifications() {
+   const {notificationsEnabled} = useSettings();
+
+   useEffect(() => {
+    if (!notificationsEnabled) return;
+    if("Notification" in window) {
+        Notification.requestPermission();
+    }
+   }, [notificationsEnabled]);
+
+const notify = (message) => {
+    if (!notificationsEnabled) return;
+
+    if (Notification.permission === "granted") {
+        new Notification(message);
+    }
+};
+
+    return {notify};
+}
+
+export default useNotifications;
