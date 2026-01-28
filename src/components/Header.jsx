3a2c6046592a7fcf112237133bb63c06147dd04a
Athanas Mochama <athanasmochama253@gmail.com>
2026-01-28 17:55:19 +0300

    Build initial Components & App.jsx, Index.css, Main.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <header className="header">
+        <h1>Priorify</h1>
+        <p>Organize your tasks by priority</p>
+      </header>
+    );
+  }
+  
+  export default Header;
---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function TaskForm({ onAdd }) {
+  const [title, setTitle] = useState("");
+  const [priority, setPriority] = useState("Medium");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!title) return;
+
+    onAdd({
+      id: Date.now(),
+      title,
+      priority,
+      completed: false,
+    });
+
+    setTitle("");
+  }
+
+  return (
+    <form className="task-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="New task..."
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
+
+      <select value={priority} onChange={e => setPriority(e.target.value)}>
+        <option>High</option>
+        <option>Medium</option>
+        <option>Low</option>
+      </select>
+
+      <button>Add Task</button>
+    </form>
+  );
+}
+
+export default TaskForm;
